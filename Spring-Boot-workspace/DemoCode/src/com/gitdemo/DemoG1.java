121af927a5d4a1c35502c8fe24239379090a6c16
ADMIN <ADMIN@DESKTOP-0QR97QM>
2021-10-22 12:49:29 +0530

    committed

--- Spring-Boot-workspace/DemoCode/src/com/gitdemo/DemoG1.java
+++ Spring-Boot-workspace/DemoCode/src/com/gitdemo/DemoG1.java
@@ -1,7 +1,13 @@
 package com.gitdemo;
 
+import java.util.Scanner;
+
 public class DemoG1 {
 	public static void main(String[] args) {
 	System.out.println("welcome to github demo");
+	Scanner s=new Scanner(System.in);
+	int a,b;
+ //int c=a+b;
+	System.out.println();
 }
 }
